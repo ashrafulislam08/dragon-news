2036b48f035a5e9a1836062df30e187ced248502
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-11-13 22:36:57 +0600

    Added a latest heading section

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const LatestNews = () => {
-  return <div>LatestNews</div>;
+  return (
+    <div className="flex gap-3 bg-gray-200 items-center p-3">
+      <p className="bg-[#D72050] text-white px-5 py-2 text-lg font-semibold">
+        Latest
+      </p>
+      <marquee behavior="" direction="" className="font-bold text-xl">
+        Match Highlights: Germany vs Spain — as it happened ! Match Highlights:
+        Germany vs Spain as...
+      </marquee>
+    </div>
+  );
 };
 
 export default LatestNews;
