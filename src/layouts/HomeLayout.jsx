fe54d624d685601a9df5dcc81d82f70fe64c8c38
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-11-14 14:21:32 +0600

    Added left side bar

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import Navbar from "../components/Navbar";
+import LeftNavbar from "../components/layout-component/LeftNavbar";
 
 const HomeLayout = () => {
   return (
@@ -14,7 +15,9 @@ const HomeLayout = () => {
         <Navbar />
       </nav>
       <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
-        <aside className="col-span-3">Left side</aside>
+        <aside className="col-span-3">
+          <LeftNavbar />
+        </aside>
         <section className="col-span-6">Main content</section>
         <aside className="col-span-3">right side</aside>
       </main>
